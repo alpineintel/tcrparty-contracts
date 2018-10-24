0f6a3a61bcb44aa051229bd5c3bbd51ee45a66f5
Steve Gattuso <steve@stevegattuso.me>
2018-10-24 15:25:41 -0400

    provisions tcr

---
+++ migrations/7_deploy_registry.js
@@ -0,0 +1,29 @@
+const TCRPartyPoints = artifacts.require('./TCRPartyPoints.sol');
+const RegistryFactory = artifacts.require('./RegistryFactory.sol');
+
+module.exports = function (deployer, network, accounts) {
+  return RegistryFactory.deployed((instance) => {
+    return instance.newRegistryBYOToken(
+      TCRPartyPoints.address,
+      [
+        500 * (10 ** 15),       // minDeposit
+        15000000 * (10 ** 15),  // pMinDeposit
+        172800,                 // applyStageLen
+        15000000 * (10 ** 15),  // pApplyStageLen
+        172800,                 // commitStageLen
+        15000000 * (10 ** 15),  // pCommitStageLen
+        64800,                  // revealStageLen
+        15000000 * (10 ** 15),  // pRevealStageLen
+        75,                     // dispensationPct
+        15000000 * (10 ** 15),  // pDispensationPct
+        50,                     // voteQuorum
+        15000000 * (10 ** 15),  // pVoteQuorum
+        172800,                 // exitTimeDelay
+        172800,                 // exitPeriodLen
+      ],
+      'TCR Party',
+    );
+  }).then((registry) => {
+    console.log(registry.address);
+  });
+};
